bd22ebb578e5faf2c625f993c5617d96314667d1
makabe0817 <131425399+makabe0817@users.noreply.github.com>
2023-06-12 15:36:25 +0900

    6/12

---
+++ Unity_tomita/otogame/Assets/Resources/Notes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    //ノーツのスピードを設定
+    int NoteSpeed = 5;
+    void Update()
+    {
+        //ノーツを移動させる
+        transform.position -= transform.up * Time.deltaTime * NoteSpeed;
+    }
+}
