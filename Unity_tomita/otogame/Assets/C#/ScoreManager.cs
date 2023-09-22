570b09c2ec998af368199b143d9c8c1ec0f37c84
hayato525 <131422083+hayato525@users.noreply.github.com>
2023-09-22 12:41:35 +0900

    9/22

---
+++ Unity_tomita/otogame/Assets/C#/ScoreManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    Text textObj;
+
+    int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void AddScore(int point)
+    {
+        score += point;
+        textObj.text = "" + score;
+    }
+}
