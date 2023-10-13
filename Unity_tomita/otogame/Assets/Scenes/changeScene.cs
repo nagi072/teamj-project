d228a0cf5181fef4d2485b2775357944323a7e96
yuuta1227 <131423151+yuuta1227@users.noreply.github.com>
2023-10-13 12:16:59 +0900

    fa

---
+++ Unity_tomita/otogame/Assets/Scenes/changeScene.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public void change_Button()
+    {
+        SceneManager.LoadScene("scenekirkaetesuto");
+    }
+}
+  
