6bb0cc69e7cd8829ee0424162546a3de2494d10a
nagi072 <131424228+nagi072@users.noreply.github.com>
2023-11-02 12:39:46 +0900

    upd

--- Unity_tomita/otogame/Assets/Scenes/toFinish.cs
+++ Unity_tomita/otogame/Assets/Scenes/toFinish.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class toFinish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+     private void Start()
     {
-        
+        // コルーチンの起動
+        StartCoroutine(DelayCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    // コルーチン本体
+    private IEnumerator DelayCoroutine()
     {
-        
+        transform.position = Vector3.one;
+
+        // 3秒間待つ
+        yield return new WaitForSeconds(3);
+
+        SceneManager.LoadScene("Men 2yu");
     }
 }
