84b218233f4d9ccb6e46ebcb7b726bd505212f35
nagi072 <131424228+nagi072@users.noreply.github.com>
2023-11-09 12:35:02 +0900

    upd

---
+++ Unity_tomita/otogame/Assets/Scenes/changeScene1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene1 : MonoBehaviour
+{
+    public void change_Button()
+    {
+        SceneManager.LoadScene("men2yu");
+    }
+}
+  
