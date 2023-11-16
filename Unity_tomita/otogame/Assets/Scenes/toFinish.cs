bdf42263216c2baf19e8ca55759b12442ceca867
nagi072 <131424228+nagi072@users.noreply.github.com>
2023-11-16 11:58:07 +0900

    upd

--- Unity_tomita/otogame/Assets/Scenes/toFinish.cs
+++ Unity_tomita/otogame/Assets/Scenes/toFinish.cs
@@ -18,7 +18,7 @@ public class toFinish : MonoBehaviour
     private IEnumerator owari()
     {
         // 3•bŠÔ‘Ò‚Â
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(25f);
 
         SceneManager.LoadScene("rizaruto");
     }
