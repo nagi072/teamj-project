4a2ed9885e5df4174be83108fc230ebec92e6270
hayato525 <131422083+hayato525@users.noreply.github.com>
2023-11-20 14:58:00 +0900

    11/20

--- Unity_tomita/otogame/Assets/Resources/script/MusicManager2.cs
+++ Unity_tomita/otogame/Assets/Resources/script/MusicManager2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : MonoBehaviour
+public class MusicManager2 : MonoBehaviour
 {
     AudioSource audio;
     AudioClip Music;
